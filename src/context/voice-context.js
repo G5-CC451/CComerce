1896635cee960eeefaa205c826336095fb5df7de
Jhonny Lorenzo <jhonny.lorenzo.r@uni.pe>
2022-07-18 13:04:54 -0500

    feat: added logger to transcript for validation in production enviroment

--- src/context/voice-context.js
+++ src/context/voice-context.js
@@ -1,71 +1,73 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import SpeechRecognition, {
   useSpeechRecognition,
-} from "react-speech-recognition";
+} from 'react-speech-recognition'
 // Configs
-import { allVoiceCommands } from "@/config/voice";
+import { allVoiceCommands } from '@/config/voice'
 
-const VoiceContext = React.createContext();
-const { Provider } = VoiceContext;
+const VoiceContext = React.createContext()
+const { Provider } = VoiceContext
 
 const VoiceProvider = ({ children }) => {
   const { transcript, resetTranscript } = useSpeechRecognition({
     commands: allVoiceCommands,
-  });
-  const [isListening, setIsListening] = useState(false);
-  const [active, setIsActive] = useState(false);
-  const [microphoneRef, setMicrophoneRef] = useState(null);
+  })
+  const [isListening, setIsListening] = useState(false)
+  const [active, setIsActive] = useState(false)
+  const [microphoneRef, setMicrophoneRef] = useState(null)
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-    return "El navegador no soporta la interacción por voz. Por favor, intente entrar usando Google Chrome.";
+    return 'El navegador no soporta la interacción por voz. Por favor, intente entrar usando Google Chrome.'
   }
 
   const handleListing = (currMicrophoneRef) => {
-    setMicrophoneRef(currMicrophoneRef);
-    setIsListening(true);
-    setIsActive(true);
+    setMicrophoneRef(currMicrophoneRef)
+    setIsListening(true)
+    setIsActive(true)
     if (currMicrophoneRef.current) {
-      currMicrophoneRef.current.style.color = "green";
+      currMicrophoneRef.current.style.color = 'green'
       SpeechRecognition.startListening({
         continuous: true,
-        language: "es-PE",
-      });
+        language: 'es-PE',
+      })
     }
-  };
+  }
 
   const stopHandle = () => {
-    setIsListening(false);
-    setIsActive(false);
+    setIsListening(false)
+    setIsActive(false)
     if (microphoneRef.current) {
-      SpeechRecognition.stopListening();
+      SpeechRecognition.stopListening()
     }
-  };
+  }
 
   const toogleListening = (currMicrophoneRef) => {
-    setMicrophoneRef(currMicrophoneRef);
-    setIsListening(!active);
-    setIsActive(!active);
+    setMicrophoneRef(currMicrophoneRef)
+    setIsListening(!active)
+    setIsActive(!active)
     if (currMicrophoneRef.current) {
       if (!active) {
-        currMicrophoneRef.current.style.color = "green";
+        currMicrophoneRef.current.style.color = 'green'
         SpeechRecognition.startListening({
           continuous: true,
-          language: "es-PE",
-        });
+          language: 'es-PE',
+        })
       } else {
-        currMicrophoneRef.current.style.color = "yellow";
-        SpeechRecognition.stopListening();
+        currMicrophoneRef.current.style.color = 'yellow'
+        SpeechRecognition.stopListening()
       }
     }
-  };
+  }
 
   const handleReset = () => {
-    stopHandle();
-    resetTranscript();
+    stopHandle()
+    resetTranscript()
     setTimeout(() => {
-      handleListing();
-    }, 500);
-  };
+      handleListing()
+    }, 500)
+  }
+
+  console.log('transcript', transcript)
 
   return (
     <Provider
@@ -81,7 +83,7 @@ const VoiceProvider = ({ children }) => {
     >
       {children}
     </Provider>
-  );
-};
+  )
+}
 
-export { VoiceContext, VoiceProvider };
+export { VoiceContext, VoiceProvider }
