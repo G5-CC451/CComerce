2cdcb7fd762281074ae5d30bf8c7d2240cd5d2ab
Jhonny Lorenzo <jhonny.lorenzo.r@uni.pe>
2022-07-19 19:37:27 -0500

    fix: clean logger in cloudinary file

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,15 +1,10 @@
 import { buildUrl } from 'cloudinary-build-url'
 
 export function cloudinaryUrl(public_id, width, height) {
-  console.log('public_id', public_id)
-  const url = buildUrl(public_id, {
+  return buildUrl(public_id, {
     cloud: {
       cloudName: 'dncxq11ii',
     },
     transformations: { width, height },
   })
-
-  console.log('url', url)
-
-  return url
 }
