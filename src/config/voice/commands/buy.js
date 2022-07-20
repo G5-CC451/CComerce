3c428a8c0d492f4f3597829b11dedb3e1829510e
Kevin Paima M <kpaimam@uni.pe>
2022-07-20 11:58:14 -0500

    add: more syntaxis voice commands buy.js

--- src/config/voice/commands/buy.js
+++ src/config/voice/commands/buy.js
@@ -16,6 +16,7 @@ export const select_to_product = getSimilars([
 
 // Para agregar un producto a carrito
 export const add_to_cart = getSimilars([
+  'añadir al carrito',
   'agrega a carrito',
   'agregar a carrito',
   'agrega a carrito de compras',
@@ -34,6 +35,7 @@ export const add_to_cart = getSimilars([
   'agregar * * a carrito de compras',
   'agrega * * al carrito',
   'agregar * * al carrito',
+  'añadir * * al carrito',
   'agrega * * al carrito de compras',
   'agregar * * al carrito de compras',
 ])
@@ -42,19 +44,28 @@ export const add_to_cart = getSimilars([
 export const increase_quantity_product = getSimilars([
   'agrega cantidad del producto',
   'uno más',
+  'agrega uno más',
+  'agrega 1 más',
+  'añade 1 más',
+  'añade uno más',
+  '1 más',
   'dale otro más',
 ])
 
 // Para aumentar los productos del carrito
 export const decrease_quantity_product = getSimilars([
   'quita cantidad del producto',
+  'quita 1',
+  'quita uno',
   'uno menos',
+  '1 menos',
   'dale otro menos',
 ])
 
 // Para comprar un producto
 export const buy_product = getSimilars([
   'compra el siguiente producto',
+  'comprar producto',
   'compra el producto',
   'comprar el producto',
   'compra ahora',
